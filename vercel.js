64ed94db8f5ad98435ef1a53f176dc8e303ca825
Gabriel Lima <102554501+Gabriellm-dev@users.noreply.github.com>
2024-01-23 15:23:28 -0300

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,9 @@
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "C:\\Workspace\\t6-one-alura\\Iniciante em Programação\\js-curso-2-aula1\\js-curso-2-aula_-",
+      "use": "@vercel/static-build"
+    }
+  ]
+}
