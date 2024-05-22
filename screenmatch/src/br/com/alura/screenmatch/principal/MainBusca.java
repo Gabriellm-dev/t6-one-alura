106c5d27ed2046b8417ee5b86300a09931cd29cb
gabriellm.dev <gabriellm.dev@gmail.com>
2024-05-22 10:53:12 -0300

    I added a question to search for the desired film

--- screenmatch/src/br/com/alura/screenmatch/principal/MainBusca.java
+++ screenmatch/src/br/com/alura/screenmatch/principal/MainBusca.java
@@ -5,12 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = sc.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=5e55c9e8" ;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=5e55c9e8"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
