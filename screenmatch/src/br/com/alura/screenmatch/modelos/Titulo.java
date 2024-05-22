9652f86c639bb3143e4fa23c44ab9bac3501821a
gabriellm.dev <gabriellm.dev@gmail.com>
2024-05-22 15:43:30 -0300

    list of movies in json

--- screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -85,8 +85,8 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public String toString() {
-        return  "nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento +
-                ", duracao= " + duracaoEmMinutos;
+        return  "( nome  = " + nome +
+                ", anoDeLancamento = " + anoDeLancamento +
+                ", duracao = " + duracaoEmMinutos + " )";
     }
 }
